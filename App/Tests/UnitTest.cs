9f5492e7a2995f9b1255910e4a485e9c90446843
252956 Tymon Smykowski <252956@edu.p.lodz.pl>
2025-03-25 21:54:09 +0100

    Poprawienie struktury aplikacji

--- App/Tests/UnitTest.cs
+++ App/Tests/UnitTest.cs
@@ -3,11 +3,12 @@ namespace Tests
 
     public class UnitTest
     {
-
+        
         [Fact]
         public void Test1()
         {
             Assert.Equal(1, 1);
         }
+        
     }
 }
