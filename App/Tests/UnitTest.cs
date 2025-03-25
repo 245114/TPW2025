d2299b4d6524cb60aa478470e00b026593f45dd0
252956 Tymon Smykowski <252956@edu.p.lodz.pl>
2025-03-25 22:43:57 +0100

    Poprawienie struktury aplikacji

--- App/Tests/UnitTest.cs
+++ App/Tests/UnitTest.cs
@@ -1,12 +1,20 @@
+using Data;
+using Logic;
+using System.Threading;
+
 namespace Tests
 {
 
     public class UnitTest
     {
         
+
         [Fact]
-        public void Test1()
+        public void Test()
         {
+            /*
+            Data ball = new Ball(10, 20, 30);
+            */
             Assert.Equal(1, 1);
         }
         
